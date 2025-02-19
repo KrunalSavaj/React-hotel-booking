df4ae81f5c41731803b2f700e901faf90e1b7313
swaminarayan <krunalsavaj4@gmail.com>
2025-02-19 19:49:38 +0530

    final commit

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export function useOutsideClick(close, ref) {
+    useEffect(
+        function () {
+            function handleClick(e) {
+                // e .stopPropagation();
+                if (ref.current && !ref.current.contains(e.target)) {
+                    close();
+                }
+            }
+
+            document.addEventListener("click", handleClick, true);
+
+            return () =>
+                document.removeEventListener("click", handleClick, true);
+        },
+        [close, ref]
+    );
+}
